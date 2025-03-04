e5096e64fedde2f3409d4ee6dfe52376bcf8e4c6
lincyrajasekaran3 <hp@DESKTOP-6433J6U>
2025-03-04 10:48:48 +0530

    saved

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Lincy </h1>
+      <p>Contact:123456789</p>
+    </div>
+  );
+}
+
+export default App;
